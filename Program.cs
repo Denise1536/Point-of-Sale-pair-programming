5cca9ed83131e31b4920a20c329395ffbc2f51c1
Denise <119461139+Denise1536@users.noreply.github.com>
2023-06-02 21:23:44 -0400

    A bit more

--- Program.cs
+++ Program.cs
@@ -10,7 +10,9 @@ double grandTotal = -1;
 
 
 //Present the menu to the user, and let them choose an item (by number or letter)
+
 //Allow the user to choose quantity ordered
+
 //Give the user a line total (item price * quantity)
 
 
@@ -19,14 +21,16 @@ double grandTotal = -1;
 
 
 //Give the subtotal, sales tax, and grand total (rounding issues/math library)
-
 salesTax = subTotal * 0.06;
 grandTotal = subTotal + salesTax;
+Console.WriteLine($"Your total order comes to ${subTotal}, plus ${salesTax} in tax, for a grand total of ${grandTotal}.");
+
 
 
 //Ask for payment type
 Console.WriteLine("Enter the number that matches your payment method: 1: Credit Card; 2: Cash; 3: Check");
 string paymentChoice = Console.ReadLine().ToLower();
+//TO DO: make a loop to error check user entry.
 switch (paymentChoice)
 {
     case "1":
@@ -38,16 +42,17 @@ switch (paymentChoice)
         break;
 
     case "3":
-    //trigger make receipt with check
-    break;
+        //trigger make receipt with check
+        break;
 
     default:
-        Console.WriteLine("");
+        Console.WriteLine("That is not a valid option.");
+        break;
 }
 
 
 //Display a receipt with items ordered, subtotal, grand total, payment info
-//Use class: ReceiptMaker
+//This will happen based on the switch case above. 
 
 
 //Return to the original menu for a new order
--- ReceiptMaker.cs
+++ ReceiptMaker.cs
@@ -8,8 +8,9 @@ namespace Point_of_Sale_Terminal_project
 {
     internal class ReceiptMaker
     {
-
-        public string MakeReceipt(string paymentChoice, double grandTotal)
+        //To create the receipt with items ordered, subtotal, grand total, payment info
+        //TO DO: pass in list with order & add it to each case
+        public string MakeReceipt(string paymentChoice, double grandTotal, double salesTax, double subTotal)
         {
 
         switch (paymentChoice)
@@ -17,19 +18,28 @@ namespace Point_of_Sale_Terminal_project
             case "1":
                 ProcessCash Cash = new ProcessCash();
                 string cashOutput = Cash.GetPayment(grandTotal);
-                Console.WriteLine($"{cashOutput}");
+                    Console.WriteLine($"Your subtotal is ${subTotal}; your grand total is ${grandTotal}.");
+                    Console.WriteLine($"Payment Info: {cashOutput}");
                 break;
 
             case "2":
                 ProcessCard Card = new ProcessCard();
                 string cardOutput = Card.GetPayment(grandTotal);
-                Console.WriteLine($"{cardOutput}");
+                    Console.WriteLine($"Your subtotal is ${subTotal}; your grand total is ${grandTotal}.");
+                    Console.WriteLine($"Payment Info: {cardOutput}");
                 break;
 
             case "3":
                 ProcessCheck Check = new ProcessCheck();
                 string checkOutput = Check.GetPayment(grandTotal);
+                    Console.WriteLine($"Your subtotal is ${subTotal}; your grand total is ${grandTotal}.");
+                    Console.WriteLine($"Payment Info: {checkOutput}");
                 break;
+
+            default:
+                    //user shouldn't see this...just making visual studio happy
+                    Console.WriteLine($"Your subtotal is ${subTotal}; your grand total is ${grandTotal}.");
+                    break;
             }
         }
     }
