11c3688f299dc9c5b8b930bc5f3a3423dd59ee7e
Jennifer <jenniferrowland1987@gmail.com>
2023-06-04 18:51:54 -0400

    added validation to cash and check

--- ProcessCheck.cs
+++ ProcessCheck.cs
@@ -8,12 +8,28 @@ namespace Point_of_Sale_Terminal_project
 {
     internal class ProcessCheck : PaymentMethods
     {
+        public bool IsValidCheckNumber(string checkNumber)
+        {
+            if (checkNumber.All(char.IsDigit) && !string.IsNullOrWhiteSpace(checkNumber))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
 
        public override string GetPayment(double grandTotal)
         {
             Console.WriteLine("What is the check number?");
             string checkNumber = Console.ReadLine();
-            return $"You paid with check number {checkNumber}.";
+            while(!IsValidCheckNumber(checkNumber))
+            {
+                Console.WriteLine("That is not a valid check number. Please try again, entering only numbers:");
+                checkNumber = Console.ReadLine();
+            }
+            return $"You paid ${grandTotal} with check number {checkNumber}.";
         }
 
 
